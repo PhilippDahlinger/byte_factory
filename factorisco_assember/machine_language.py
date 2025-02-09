199f2adc61babb650a1bf945e4b364b4077e8d9f
Philipp Dahlinger <philipp.dahlinger@gmx.net>
2025-02-09 23:44:46 +0100

    started example code for factorisco

---
+++ factorisco_assember/machine_language.py
@@ -0,0 +1,7 @@
+code = [
+    {"opcode": 1, "rd": 1,"add_opcode": 1, "rs1": 1,"rs2": None, "imm": 1},
+    {"opcode": 1, "rd": 2,"add_opcode": 1, "rs1": 2,"rs2": None, "imm": 1},
+    {"opcode": 1, "rd": 1,"add_opcode": 2, "rs1": 1,"rs2": 2, "imm": None},
+    {"opcode": 1, "rd": 2,"add_opcode": 2, "rs1": 1,"rs2": 2, "imm": None},
+    {"opcode": 1, "rd": 3,"add_opcode": None, "rs1": None,"rs2": None, "imm": -2},
+]
