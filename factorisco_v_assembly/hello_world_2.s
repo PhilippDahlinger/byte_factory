62592996ab599bf0da879fe6194095f5fb28ec03
Philipp Dahlinger <philipp.dahlinger@gmx.net>
2025-02-26 17:25:40 +0100

    display test

--- factorisco_v_assembly/hello_world_2.s
+++ factorisco_v_assembly/hello_world_2.s
@@ -4,9 +4,16 @@
 _start:
 	# reset display
 	sw zero, 10(zero)
-	# cursor at (0, 0)
-	sw zero, 5(zero)
+	# cursor at (5, 0)
+	li t0, 5
+	sw t0, 5(zero)
 	sw zero, 6(zero)
+	# shift of cursor set to 8
+	li t0, 8
+	sw t0, 13(zero)
+	# enable wrap
+	#li t0, 1
+	sw zero, 14(zero)
 	# fdr to 0 to show first line
 	sw zero, 9(zero)
 	li t0, 72 # H
@@ -31,6 +38,20 @@ _start:
 	sw t0, 7(zero)
 	li t0, 68 # D
 	sw t0, 7(zero)
+	# refresh display
+	sw zero 12(zero)
+	# sleep
+	li a0 10
+	call _sleep
+	li t0, 1
+	sw t0, 9(zero)
+	sw zero 12(zero)
+	li a0 10
+	call _sleep
+	# clear row
+	sw zero 11(zero)
+	# refresh
+	sw zero 12(zero)
 	halt
 	nop
 	nop
@@ -39,4 +60,13 @@ _start:
 	nop
 	nop
 	nop
+
+_sleep:
+	beq a0, zero, _end_loop
+	subi a0, a0, 1
+	j _sleep
+_end_loop:
+	ret
+	
+	
 	
---
+++ factorisco_v_assembly/kernel/ecall_01.s
@@ -0,0 +1,6 @@
+# ECALL implementation
+.text
+
+.data
+
+
