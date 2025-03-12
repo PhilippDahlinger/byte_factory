3db23823bc17a75b94fbf4e5ee837f8535cfa34d
Philipp Dahlinger <philipp.dahlinger@gmx.net>
2025-03-13 00:39:16 +0100

    huge process in minesweeper. Have a display, a cursor, bomb generation and a status array. Only left is implementing space action.

--- factorisco_v_assembly/minesweeper.s
+++ factorisco_v_assembly/minesweeper.s
@@ -33,17 +33,16 @@ _start:
 	beq a0, t0, 2f # down
 	li t0, 9
 	beq a0, t0, 3f # up
+	li t0, 32
+	beq a0, t0, space_event # space
 	j game_loop # none of these keys
 	# key event handlings
+	# directions:
 	0:
 	li a0, 0
 	li a1, -1
 	j cursor_movement
 	1:
-	# debug print
-	#li a0, 82 #"R"
-	#li a7, 18
-	#ecall
 	li a0, 0
 	li a1, 1
 	j cursor_movement
@@ -55,13 +54,15 @@ _start:
 	li a0, -1
 	li a1, 0
 	j cursor_movement
+	space_event:
+	
+	
 
 	
 	cursor_movement:
 	# new potential cursor position
 	add s5, s1, a0
 	add s6, s2, a1
-	
 	
 	# check if it is in bounds
 	blt s5, zero, 1f
@@ -141,6 +142,22 @@ init:
 	push a0 # save ref
 	li a1, 0
 	call init_field
+	# add bombs
+	# get bomb ref back from stack
+	lw s5, 0(sp)
+	li s6, 5 # bomb counter, always >0, so can use a do-while loop
+	0:
+	mv a0, s10
+	li a7, 27
+	ecall # randint of the array size
+	add t0, a0, s5 # array access
+	lw t1, 0(t0) # bombs[t0]
+	beq t1, s8, 0b # start again if bomb is already there
+	# place bomb
+	sw s8, 0(t0)
+	dec s6
+	bne s6, zero, 0b # go back to place next bomb
+	
 	# status 
 	mv a0, s10
 	li a7, 2
@@ -148,14 +165,36 @@ init:
 	push a0 # save ref
 	li a1, 136
 	call init_field
+	
 	pop s3 # status field address
 	pop s4 # bomb field address
+	
+	# DEBUG: draw bombs in status
+	li t0, 0
+	add t2, s3, s10 # end of status array
+	0:
+	add t1, s3, t0
+	beq t1, t2, 1f
+	add t3, s4, t0 # bomb index
+	lw t3, 0(t3) # actual bomb bool
+	beq t3, s8, 2f # if 1: draw bomb in status
+	3:
+	inc t0 
+	j 0b
+	2:
+	li t3, 131 # create diamond bomb char
+	sw t3, 0(t1) # bomb char in status reg
+	j 3b
+	1:
+	# END DEBUG
+	
 	pop ra
 	ret
 
 init_field:
 	# a0: memory start, s10: size of field
 	# a1: initial value
+	# t0: end of array
 	add t0, a0, s10
 	0:
 	beq a0, t0, 1f
